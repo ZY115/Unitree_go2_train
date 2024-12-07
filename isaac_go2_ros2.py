16d94e0e3e2e6b62d0afe54fba3f167ffe344c5e
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2024-12-07 10:51:44 -0500

    add param control for sim app and fix minor issue with rtf display

--- isaac_go2_ros2.py
+++ isaac_go2_ros2.py
@@ -1,25 +1,26 @@
 from isaacsim import SimulationApp
-
-# launch omniverse app
-simulation_app = SimulationApp({"headless": False, "anti_aliasing": 0,
-                                "width": 1280, "height": 720, 
-                                "hide_ui": False})
-import rclpy
-import torch
-import omni
-import carb
-import go2.go2_ctrl as go2_ctrl
-import ros2.go2_ros2_bridge as go2_ros2_bridge
-from go2.go2_env import Go2RSLEnvCfg, camera_follow
-import env.sim_env as sim_env
-import go2.go2_sensors as go2_sensors
-import time
 import os
 import hydra
+import rclpy
+import torch
+import time
 
 FILE_PATH = os.path.join(os.path.dirname(__file__), "cfg")
 @hydra.main(config_path=FILE_PATH, config_name="sim", version_base=None)
 def run_simulator(cfg):
+    # launch omniverse app
+    simulation_app = SimulationApp({"headless": False, "anti_aliasing": cfg.sim_app.anti_aliasing,
+                                    "width": cfg.sim_app.width, "height": cfg.sim_app.height, 
+                                    "hide_ui": cfg.sim_app.hide_ui})
+
+    import omni
+    import carb
+    import go2.go2_ctrl as go2_ctrl
+    import ros2.go2_ros2_bridge as go2_ros2_bridge
+    from go2.go2_env import Go2RSLEnvCfg, camera_follow
+    import env.sim_env as sim_env
+    import go2.go2_sensors as go2_sensors
+    
     # Go2 Environment setup
     go2_env_cfg = Go2RSLEnvCfg()
     go2_env_cfg.scene.num_envs = cfg.num_envs
@@ -84,7 +85,7 @@ def run_simulator(cfg):
                 time.sleep(sleep_duration)
         actual_loop_time = time.time() - start_time
         rtf = min(1.0, sim_step_dt/elapsed_time)
-        print(f"\rStep time: {actual_loop_time*1000:.2g}ms, Real Time Factor: {rtf:.2g}", end='', flush=True)
+        print(f"\rStep time: {actual_loop_time*1000:.2f}ms, Real Time Factor: {rtf:.2f}", end='', flush=True)
     
     dm.destroy_node()
     rclpy.shutdown()
